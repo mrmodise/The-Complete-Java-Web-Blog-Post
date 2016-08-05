7ce54a98df1ccfe37a2bc3bce1afa0289c1673dd
mrmodise <modisemorebodi@gmail.com>
2016-08-05 15:06:55 +0200

    initial project commit

---
+++ src/main/java/com/mrmodise/FintechjournalApplication.java
@@ -0,0 +1,12 @@
+package com.mrmodise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechjournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechjournalApplication.class, args);
+	}
+}
