a74b955062f2250dde6bd398fd4ec113f3b3fea6
mrmodise <modisemorebodi@gmail.com>
2016-08-05 18:16:04 +0200

    correct resources files

--- src/main/java/com/mrmodise/controller/HomeController.java
+++ src/main/java/com/mrmodise/controller/HomeController.java
@@ -10,5 +10,41 @@ public class HomeController {
 	public String home(){
 		return "index";
 	}
+	
+	// author of a blog post route and mapping to view
+		@RequestMapping("/author")
+		public String author(){
+			return "author/staff-writer";
+		}
+		
+		// team members route and mapping to view
+		@RequestMapping("/our-team")
+		public String team(){
+			return "teams/team";
+		}
+		
+		// member profile route and mapping to view
+		@RequestMapping("/member-profile")
+		public String member(){
+			return "member/member-profile";
+		}
+		
+		// contact us route and mapping to view
+		@RequestMapping("/contact-us")
+		public String contactUs(){
+			return "contact-us/contact-us";
+		}
+		
+		// about us route and mapping to view
+		@RequestMapping("/about-financial-tech-journal")
+		public String aboutUs(){
+			return "about-us/about-us";
+		}
+		
+		// about us route and mapping to view
+			@RequestMapping("/register")
+			public String register(){
+				return "register/guest-register";
+			}
 
 }
