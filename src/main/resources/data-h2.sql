b4666c2200192771a4664db481224e73e81acd5d
mrmodise <modisemorebodi@gmail.com>
2016-08-13 13:34:04 +0200

    updated displaying of posts

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -4,19 +4,21 @@ SET @TEASER = 'Businesses operating in the modern economy find themselves provid
 
 SET @BODY = 'Marketing does not refer to promotional activities as TV adverts, social media, billboards and so forth that comes to one’s mind when they hear the word Marketing. Marketing is a ‘competitive strategy’ that companies employ to compete successfully and gain market share. It is a way of conducting business operations and thinking. The one methods that will elevate your career to new heights if you only adopt it. 
 <br />
+<br />
 MARKETING STEPS
+<br />
 <br />
 Successful firms such as Apple first study the customer. Before an iPhone is designed, the concept, Apple first conducts a market research to find the specifications that an iPhone user expects on their phone. They do not model a phone ‘that they think customers would love’ but rather find what the customers love and want and then model it into a product.They try to find out what colour, what screen size, what browser options  and then build their iPhones based on these customer requirements. This is different from Apple trying to make a product that they believe is a great product and then trying to ‘force it’ to customers hands as firms such as Blackberry have done and have paid for that. The first step to any Marketing strategy is to study customer requirements, what customers want, how they want it and then design or make your products based on these requirements. This is the concept of Marketing that i would like you to adopt in your own career strategy.
 
-If you adopt this strategy, view your potential employers as your clients or customers. You are the corporate (‘your name’ PTY LTD) and they (employers) are your ‘customers.’ You are a service provider in this case. Your ‘products’ include medical services if you are a Medical Doctor or software if you are a computer scientist. Now make your customer requirements the center of your career planning and management. First find what industry you are in, what do employers want from job applicants in that industry? What are the trends? You are now doing what successful companies have termed “Market research”. What qualifications, skills and competencies are trending in your industry? What do your customers (potential/current employers) want from you? If you are working already, why is your current employer doing business with you? What attracts him to be your customer? How do should you market yourself (CV)? What is your personal logo? What is your mission, vision? Who are your shareholders (mentors)? What is your share price in the market (How much are your currently earning) ? Funny but powerful!
+If you adopt this strategy, view your potential employers as your clients or customers. You are the corporate (‘your name’ PTY LTD) and they (employers) are your ‘customers.’ You are a service provider in this case. Your ‘products’ include medical services if you are a Medical Doctor or software if you are a computer scientist. <br/><br/>Now make your customerNow make your customer requirements the center of your career planning and management. First find what industry you are in, what do employers want from job applicants in that industry? What are the trends? You are now doing what successful companies have termed “Market research”. What qualifications, skills and competencies are trending in your industry? What do your customers (potential/current employers) want from you? If you are working already, why is your current employer doing business with you? What attracts him to be your customer? How do should you market yourself (CV)? What is your personal logo? What is your mission, vision? Who are your shareholders (mentors)? What is your share price in the market (How much are your currently earning) ? Funny but powerful!
 
-Once you have adopted this strategy it will surely increase your competitive edge next time you are in the market as it has done for successful firms the world over. Many big brands have argued this customer-focus mindset, saying it wastes time and is irrelevant for their business no matter what statistics and practice shows (Kodak vs Instagram; YouTube has democratized  learning, google has democratized information; eBay has democratized commerce and these are all examples of companies that have embraced a marketing oriented approach and have become successful in doing that. It starts with the desire to make customer requirements the overriding goals in your business. what do your customers want? This business spend millions sometimes billions doing this market research because it is important to them. Why don’t you do the same? Why don’t you spend time and money to find what industry you are in and how it is changing? What do employers seek from applicants and what are the trends in that area? What qualifications are being required and what makes employers happy? This might sound like a simple philosophy so implement but many business have failed in that attempt and those who have successfully implemented have been rewarded hugely for that.';
+Once you have adopted this strategy it will surely increase your competitive edge next time you are in the market as it has done for successful firms the world over. <br/><br/>Many big brands have argued this customer-focus mindset, saying it wastes time and is irrelevant for their business no matter what statistics and practice shows (Kodak vs Instagram; YouTube has democratized  learning, google has democratized information; eBay has democratized commerce and these are all examples of companies that have embraced a marketing oriented approach and have become successful in doing that. It starts with the desire to make customer requirements the overriding goals in your business. what do your customers want? This business spend millions sometimes billions doing this market research because it is important to them. Why don’t you do the same? Why don’t you spend time and money to find what industry you are in and how it is changing? What do employers seek from applicants and what are the trends in that area? What qualifications are being required and what makes employers happy? This might sound like a simple philosophy so implement but many business have failed in that attempt and those who have successfully implemented have been rewarded hugely for that.';
 
 SET @TEASER1 = 'So this is like my first blog ever. I made it to my first! More of such blogs to come you know. Great stuff i tell you
 
 And then where do  i start? I picked “Touch Typing” as the topic to my first blog because i thought it would be easy to do and also most people really need to learn how to touch type guys because it would really, really assist them in their daily chores and stuff they do everyday that involves typing anything into a computer. So to give you an example i was given a 1000+row spreadsheet to key into a computer at work and i completed all that task in about 15mins, keying all the data into the system. Writing this blog will probably take me some 5 mins too. And also the other time i was in the library and i was typing and i was sitting next to this girl, and i swear she wanted to get my number. So touch typing can really payoff guys. And then you can also manage your blogs and also type in things easily for other people. Like some schools stuff and so forth…';
 
-SET @BODY1 = 'So let’s start: <br />
+SET @BODY1 = 'So let’s start: <br /> <br />
 
 Well i once was a “hunt and pecker” too. That means  i typed like most folks do, you know using two or three fingers to hit all the keys. It was tiring to say the least and it took me a long time to get anything done. It would take me a long long time to write anything up like CV or Business Plan.  That’s what hunt and peck typing did for me. 
 
@@ -27,9 +29,9 @@ And then i learnt how to touch type. What is touch typing?  Touch typing is typi
 And just so you know i am typing this blog in the dark. I am not at all looking at my keyboard. I am just looking at the screen and enjoying myself. So touch typing is an invaluable skill to learn. Very important. And when i first learnt to touch type i thought i would never learn how to touch type, it was really hard, so hard i never thought i would get it all. I thought it wasn’t meant for some people and i couldn’t do it.
 
 How did i learn? Well i used this site keybr this is a great site for getting the basics to touch typing. I  used this one as my first and then i moved to this app that’s free now called Typing Master. That’s the name of the app Typing Master and i think you can find it here. It’s a free app that teaches you how to touch type and it is really really good. Like it is what taught me how to touch type.  So  first i used the keybr.com site which is free and then i downloaded the Typing Master app which is free as well and so that’s how i learnt to touch type. And then keybr.com only obviously works on the web, like you need an internet connection and then as for Typing Master you can download it and then use it when offline.
-<br />
+<br /><br />
 Some Tips on Touch Typing:
-<br />
+<br /><br />
 It’s going to be irritating the first few months and you’ll often think that you won’t make it. But then so is learning anything in life that really pays off, it is never easy at first and so is touch typing.
 You speed will initially be slower than when typing with only two fingers and not looking at the screen. So at first you’ll be typing really slowly maybe a speed of 10wpm and that is where i stared too. I started at 10wpm, then 18wpm and then after some time 23wpm and then to 30wpm and it keeps growing until i am at 80wpm now.
 Don’t give up. As with anything in life, never give up on yourself. It might be hard when you start, and it’s quite hard when you start to learn anything, but it will pay of very well for you. So just stick to it and make sure you learn how to type properly…
@@ -37,14 +39,14 @@ Practice everyday for atleast for 2 hours minimum. Practice more hours if you ca
  
 So happy touch typing and this skill will really pay off for you, trust me guys it has for me-a lot!';
 
-SET @TEASER3 = 'Why Study CIMA?
-
+SET @TEASER3 = '<b>Why Study CIMA?</b>
+<br /><br />
 CIMA  (or Chartered Institute of Management Accounting) is recognised world wide as the best qualification in business and finance and combines Accounting, Finance and Financial Management topics to produce professionals that are well equiped to drive business results. The CIMA qualification is more than just an Accounting qualification. In fact If you want to be an Accountant and solely that, then don''t consider CIMA. But if you are looking for a business qualification with robust assessments and syllabus, then CIMA is the right qualification for you.';
 
 SET @BODY3 = 'The CIMA syllabus is very strong on core Accounting skills while it also includes Operations Management, Marketing, Sustainability, Strategic Management. We also study Costing, Financial management and Financial Reporting. So you''re looking for a qualification that produces well rounded business professionals then CIMA qualification is the right qualification for you.
-
-EXAMINATIONS AND LEVELS
-
+<br /><br />
+<b>EXAMINATIONS AND LEVELS</b>
+<br /><br />
 The CIMA qualification has three levels on its professional qualification these are:
 
 the Operational Level, Management Level and finally the Strategic Level. The reason for such classification is because there re three levels in every enterprise: the operational level is the stuff that does the daily production or delivering services.
@@ -56,41 +58,41 @@ The last level of the CIMA qualification is the Strategic Level. These are the g
 Doing CIMA myself at Botswana Accountancy College, CIMA has molded me in to an all rounded professional who is well conversant with all areas of business. I have learnt so many things during this course and has applied the techniques learnt to improve the bottom line at my current employer.
 
 How many papers are there on CIMA? CIMA has three modules at each of the  three levels: operational, management and Srategic levels.That makes a total of 9 modules in the CIMA qualification. But note that there 12 examinations in this programme, because at each level i.e operational level, management level, strategic level, there is a Case study that integrates all the modules. So there are three case studies. CIMA Exams are all online exams and are available on demand.
-
-How much are the Exams?
-
+<br /> <br/>
+<b>How much are the Exams?</b> <br />
+<br />
 If you feel ready to write at any day you can simply book online and pay online, or go to Botswana Accountancy College, Damelin, Botho University or The Challenge Linq at The Office (near Fair grounds) and pay there. At operational level the exam fees are P1160 per paper (Botswana Accountancy College fees). The Case study for the Operational Level is P1875.
 
 At Management Levels, each exam is P1160 again and there are 3 exams, and finally for the CAse study P1875. 
 
 At strategic Level, each exam is P1875 and there are 3 exams and then the Strategic Case study is P2830. All these are Botswana Accountancy College and to my best knowledge it is the cheapest.
-
-BOOKS?
-
+<br /><br />
+<b>BOOKS?</b>
+<br /><br />
 The books are so easy to find, you can buy second hand books for about 300 or so for each level. At University of Botswana, the books are P530, band new. So you want to buy second hand books you can check the following groups on Facebook:
 
 CIMA Botswana Students
-
-Accountants (CIMA, ACCA, BICA, AAT)
-
-Tuition Fees?
-
-Botswana Accountancy College fees for the Operational, Management Levels and Strategic Levels:
-
-Operational Level per paper:  P3800
-
-Operation Level Case study:  P3800
-
-Management Level per paper: P3800
-
-Management Level Case Study:P3800
-
-Strategic Level per paper: P4390
-
-Strategic Level per Paper: P4390
-
-This fees are for part time only.
-
+<br />
+* Accountants (CIMA, ACCA, BICA, AAT)
+<br /><br />
+<b>Tuition Fees?</b>
+<br /> <br/>
+* Botswana Accountancy College fees for the Operational, Management Levels and Strategic Levels:
+<br />
+* Operational Level per paper:  P3800
+<br />
+* Operation Level Case study:  P3800
+<br />
+* Management Level per paper: P3800
+<br />
+* Management Level Case Study:P3800
+<br />
+* Strategic Level per paper: P4390
+<br />
+* Strategic Level per Paper: P4390
+<br />
+* This fees are for part time only.
+<br /><br/>
 If you need any more information please comment below and I shall provide such...';
 
 insert into post(id,image_name, title,slug,teaser,body,author_id,posted_on, draft, status) values (1,'img/cv-writing.jpg','ADOPTING BUSINESS STRATEGIES TO YOUR CAREER STRATEGY', 'business-strategy-to-your-career-strategy', @TEASER,@BODY, 1, CURRENT_TIMESTAMP, 'false', 'active');
